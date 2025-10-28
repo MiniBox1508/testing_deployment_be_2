357771c7f381813307b649c4baa356393e488023
MiniBox1508 <hungbeo7a@gmail.com>
2025-10-29 03:09:37 +0700

    okok

--- BE/app.js
+++ BE/app.js
@@ -11,6 +11,7 @@ app.use(cors());
 // --- MySQL connection (sử dụng POOL) ---
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
