1bc6d87236cdb7a88782cb24d70ae22db403e6ae
MiniBox1508 <hungbeo7a@gmail.com>
2025-10-29 03:24:13 +0700

    okok2

--- BE/app.js
+++ BE/app.js
@@ -3,10 +3,10 @@ const express = require("express");
 const mysql = require("mysql2");
 const app = express();
 const port = process.env.PORT;
-const cors = require("cors"); // <-- THÊM DÒNG NÀY
+//const cors = require("cors"); // <-- THÊM DÒNG NÀY
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
 // --- MySQL connection (sử dụng POOL) ---
 const db = mysql.createPool({
