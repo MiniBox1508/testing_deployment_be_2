ac3401c4dc1417d611ef1e20e3f72d7c0af7bd09
MiniBox1508 <hungbeo7a@gmail.com>
2025-11-17 15:11:18 +0700

    Lan 1 ngay 17-11-25

--- BE/FILE.sql
+++ BE/FILE.sql
@@ -1,6 +1,6 @@
 USE building_management;
--- DÙNG CHO US 001: cư dân
-CREATE TABLE residents (
+-- DÙNG CHO US 001: user (thay cho residents)
+CREATE TABLE user (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   full_name VARCHAR(100),
   first_name VARCHAR(100) NOT NULL,
@@ -13,6 +13,7 @@ CREATE TABLE residents (
   role VARCHAR(30),
   residency_status VARCHAR(30),
   email VARCHAR(30),
+  password VARCHAR(100) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 -- DÙNG CHO US 008 VÀ 009: thanh toán và tra cứu
@@ -32,28 +33,28 @@ CREATE TABLE payments (
   verified_at DATETIME,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
-  CONSTRAINT payments_ibfk_1 FOREIGN KEY (resident_id) REFERENCES residents(id)
+  CONSTRAINT payments_ibfk_1 FOREIGN KEY (resident_id) REFERENCES user(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
 
 describe payments;
-describe residents;
+describe user;
 -- tăt chế độ an toàn
 set sql_safe_updates = 0;
-update residents
+update user
 set full_name = concat(first_name,'',last_name);
 -- bật chế độ an toàn
 set sql_safe_updates = 1;
-INSERT INTO residents (
+INSERT INTO user (
   full_name, first_name, last_name, phone, apartment_id, 
-  state, cccd, birth_date, role, residency_status, email
+  state, cccd, birth_date, role, residency_status, email, password
 ) VALUES (
   'Đỗ Văn B', 'Văn', 'B', '0938099203', 'Tầng 7 - Phòng 713',
-  'Active', '077204000123', '1999-10-30', 'Cư dân', 'người thuê', 'dovanb@gmail.com'
+  'Active', '077204000123', '1999-10-30', 'Cư dân', 'người thuê', 'dovanb@gmail.com', 'password'
 ); 
-sELECT id, full_name FROM residents ORDER BY id DESC LIMIT 1;
+sELECT id, full_name FROM user ORDER BY id DESC LIMIT 1;
 select * from payments;
 INSERT INTO payments (
   resident_id, amount, state, transaction_ref,
@@ -84,7 +85,7 @@ SELECT
   p.payer_name,
   p.payment_date
 FROM payments p
-JOIN residents r ON p.resident_id = r.id;
+JOIN user r ON p.resident_id = r.id;
 
 UPDATE payments
 SET state = 1,
@@ -118,18 +119,18 @@ create table notifications(
 -- 5. Thông báo chung
 (NOW(), NULL, 'E5-505', 'Ban quản lý tòa nhà: Vui lòng đeo thẻ cư dân khi ra vào tòa nhà để đảm bảo an ninh.');
 select * FROM notifications;
-select * from residents;
+select * from user;
 select * from payments;
 SET sql_safe_updates = 0;
 delete from payments;
-delete from residents;
+delete from user;
 delete from notifications;
 
 SET sql_safe_updates = 1;
 SET FOREIGN_KEY_CHECKS = 0;
 -- reset auto_increment
 TRUNCATE TABLE payments;
-TRUNCATE TABLE residents;
+TRUNCATE TABLE user;
 truncate notifications;
 -- must be carefull
 -- ...existing code...
@@ -137,6 +138,6 @@ truncate notifications;
 CREATE OR REPLACE VIEW transactions AS
 SELECT id, resident_id, amount, state, transaction_ref, feetype, payment_date, payment_form, provider_tx_id, payer_account, payer_name, created_at, verified_at
 FROM payments;
-ALTER TABLE residents AUTO_INCREMENT = 1;
+ALTER TABLE user AUTO_INCREMENT = 1;
 
 SET FOREIGN_KEY_CHECKS = 1;
